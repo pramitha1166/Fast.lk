36d4def37c5cb0e6946a32fd5c6049385ac01efd
pramitha1166 <pramitha.wanigarathne@gmail.com>
2021-05-02 10:49:24 +0530

    delete | update order api

--- OrderService/src/controller/order.js
+++ OrderService/src/controller/order.js
@@ -34,3 +34,56 @@ exports.createOrder = (req,res) => {
         }
     })
 }
+
+exports.getOrderById = (req,res,next,id) => {
+    Order.findById(id).exec((err,order) => {
+        if(err) {
+            return res.status(400).json({
+                error: err
+            })
+        }else {
+            req.order = order
+            next()
+        }
+    })
+}
+
+exports.getSingleOrder = (req,res) => {
+    return res.json(req.order)
+}
+
+exports.deleteOrder = (req,res) => {
+    const order = req.order
+    order.remove((err,order) => {
+        if(err) {
+            return res.status(400).json({
+                error: err
+            })
+        }else {
+            res.json({
+                'message':'Order has been deleted',
+                order
+            })
+        }
+    })
+}
+
+exports.updateOrder = (req,res) => {
+    const order = req.order
+    order.customer = req.body.customer
+    order.amount = req.body.amount
+    order.items = req.body.items
+    order.billing = req.body.billing
+    order.save((err,order) => {
+        if(err) {
+            return res.status(400).json({
+                error: err
+            })
+        }else {
+            res.json({
+                'message': 'Order has been updated successfully!',
+                order
+            })
+        }
+    })
+}
