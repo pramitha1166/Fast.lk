fbb4cc4289cd130572c1ef58beb16a7211bde7f0
Chandula <chandula.madigasekara@gmail.com>
2021-05-03 22:04:52 +0530

    Added encrypted password and some validation for create seller

--- SellerService/src/service/sellerService.js
+++ SellerService/src/service/sellerService.js
@@ -1,16 +1,27 @@
 "use strict";
 
 const Seller = require('../model/seller');
+const { validateSeller } = require('./sellerValidation');
+const bcrypt = require('bcryptjs');
 
 const addSeller = async (req, res) => {
     const seller = req.body;
-    const newSeller = new Seller(seller);
+    let newSeller = await validateSeller(seller);
 
     try {
-        await newSeller.save();
-        res.status(201).json('New seller added'); 
+        if(newSeller === 0) {
+            res.status(400).json({ msg: 'Please enter all fields' });
+        } else if(newSeller === 1) {
+            res.status(400).json({ msg: 'User already exists' });
+        } else {
+            newSeller = new Seller(seller);
+            newSeller.password = bcrypt.hashSync(newSeller.password, 10);
+            await newSeller.save();
+            res.status(201).json({ msg:'User added' });     
+        }
     } catch (error) {
-        res.status(409).json({ message: error.message });
+        console.log(error);
+        res.status(409).json({ msg: 'User not created' });
     }
 }
 
@@ -19,7 +30,7 @@ const getSellerById = async (req, res) => {
     const currentSeller = await Seller.findById(req.params.id); 
     res.json(currentSeller);
    } catch (error) {
-       res.status(200).json('No user with given ID');
+       res.status(200).json({ msg:'No user with given ID' });
    }
 }
 
@@ -30,25 +41,25 @@ const updateSeller = async (req, res) => {
         sellerToUpdate.firstName = req.body.firstName;
         sellerToUpdate.lastName = req.body.lastName;
         sellerToUpdate.username = req.body.username;
-        sellerToUpdate.password = req.body.password;
+        sellerToUpdate.password = bcrypt.hashSync(req.body.password, 10);
         sellerToUpdate.email = req.body.email;
         sellerToUpdate.mobileNo = req.body.mobileNo;
         sellerToUpdate.address = req.body.address;
 
         await sellerToUpdate.save();
-        res.json('Seller updated');
+        res.json({ msg:'Seller updated' });
 
     } catch(error) {
-        res.status(400).json('No such user');
+        res.status(400).json({ msg:'No such user' });
     }
 }
 
 const deleteSeller = async (req, res) => {
     try{
         await Seller.findOneAndRemove(req.params.id).exec();
-        res.json('Successfully deleted');
+        res.json({ msg:'Successfully deleted' });
     } catch(error) {
-        res.json({ message:'Error! Could not delete' });
+        res.json({ msg:'Error! Could not delete' });
     }
 }
 
