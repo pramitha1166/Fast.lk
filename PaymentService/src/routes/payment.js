b530565ae5c309f78e75f58302de6bd08dd723c6
pramitha1166 <pramitha.wanigarathne@gmail.com>
2021-05-09 18:13:06 +0530

    change payment gateway

--- PaymentService/src/routes/payment.js
+++ PaymentService/src/routes/payment.js
@@ -1,16 +1,11 @@
 'use strict'
 
 const express = require('express')
+
 const router = express.Router()
 
-const {createPayment,getPayments,deletePayment,updatePayment,getPaymentById,getSinglePayment} = require('../controller/payment')
+const {createPayment,createPayments} = require('../controller/payment')
 
-router.post('/payment', createPayment)
-router.get('/payments', getPayments)
-router.get('/payment/:paymentId', getSinglePayment)
-router.delete('/payment/:paymentId', deletePayment)
-router.put('/payment/:paymentId', updatePayment)
-
-router.param('paymentId', getPaymentById)
+router.get('/payment', createPayments)
 
 module.exports = router
