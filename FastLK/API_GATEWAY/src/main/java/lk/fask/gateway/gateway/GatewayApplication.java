44402921e7f75e9e8225c3d7167241e98d0d92dc
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-05-09 07:36:09 +0530

    gateway updated

--- FastLK/API_GATEWAY/src/main/java/lk/fask/gateway/gateway/GatewayApplication.java
+++ FastLK/API_GATEWAY/src/main/java/lk/fask/gateway/gateway/GatewayApplication.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String uU(){
-		return "23323";
 	}
 }
