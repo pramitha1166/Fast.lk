8e2fa4762ed949b99b78e1e2fe0b828e41aa6c4f
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-05-14 03:43:52 +0530

    Buying page implemented

--- client/src/components/BuyItem/BuyItem.jsx
+++ client/src/components/BuyItem/BuyItem.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import LoaderSpinner from "./../Comman/LoaderSpinner";
-import { FaUndo } from "react-icons/fa";
+import {
+  AiOutlineArrowDown,
+  AiFillPlusSquare,
+  AiFillMinusSquare,
+} from "react-icons/ai";
+import { FaShoppingCart } from "react-icons/fa";
 import axios from "axios";
 import "rc-slider/assets/index.css";
 
@@ -8,22 +13,11 @@ import Pagination from "./../Comman/Pagination";
 import ItemCarousel from "./ItemCarousel";
 
 import "./../../App.css";
-import "./../../styles/Product.css";
+import "./../../styles/Buy.css";
 
 const BuyItem = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    window.scroll(0,0);
-    axios
-      .get("/api/products/view?page=1&limit=4")
-      .then((res) => {
-        setItems(res.data.result.results);
-        setIsLoading(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
 
   return (
     <>
@@ -48,40 +42,61 @@ const BuyItem = () => {
       </div>
       <div class="main main-raised">
         <div>
-          {isLoading ? (
-            <LoaderSpinner />
-          ) : (
-            <>
-              <div className="container smooth_load">
-                <div class="row">
-                  <div class="col-lg-6 col-sm-12 col-md-12">
-                    <div style={{ marginTop: 23 }}>
-                      <h4 style={{ textAlign: "left" }}>Find what you need.</h4>
-                      
-                    </div>
-                    <div>
-                      <ItemCarousel />
-                    </div>
-
-                    <div className="gap">
-                      <h6>Price Range</h6>
-                    </div>
-                    <div className="gap">
-                      <h6>Categories</h6>
+          <>
+            <div className="container smooth_load">
+              <div class="row">
+                <div class="col-lg-6 col-sm-12 col-md-12">
+                  <div style={{ padding: 50 }}>
+                    <ItemCarousel />
+                  </div>
+                </div>
+                <div class="col-lg-6 col-sm-12 col-md-12">
+                  <div style={{ marginTop: 23 }}>
+                    <h4 style={{ textAlign: "left" }}>Items Name</h4>
+                    <div className="reset">
+                      <h3>Rs 1200.00</h3>
                     </div>
                   </div>
-                  <div class="col-lg-6 col-sm-12 col-md-12">
-                    <div class="card-deck">
-                      {items.map((item) => {
-                        return <h3>Sddsd</h3>;
-                      })}
+                  <div>
+                    <div className="reset">
+                      <h6 className="text-danger">Description</h6>
+                      <AiOutlineArrowDown />
                     </div>
-                    <Pagination />
+                  </div>
+                  <h5 style={{ textAlign: "left" }}>
+                    Every landing page needs a small description after the big
+                    bold title, that&apos;s why we added this text here. Add
+                    here all the information that can make you or your product
+                    create the first impression.
+                  </h5>
+                  <div>
+                    <div className="reset">
+                      <h6 className="text-danger">Seller</h6>
+                      <h6 style={{ textAlign: "right" }}>Derby distributers</h6>
+                    </div>
+                    <div className="buyerButton">
+                      <div className="qtyButton">
+                        <AiFillMinusSquare />
+                        <h4
+                          style={{
+                            fontWeight: "bold",
+                            marginLeft: 10,
+                            marginRight: 10,
+                          }}
+                        >
+                          12
+                        </h4>
+                        <AiFillPlusSquare />
+                      </div>
+                      <button class="btn btn-danger">
+                        Add to cart <FaShoppingCart />
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </>
-          )}
+            </div>
+          </>
         </div>
       </div>
     </>
